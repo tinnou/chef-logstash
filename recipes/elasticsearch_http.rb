5b9a4e4346ef4f8db27213f02e06057e59eca7b7
tinnou <antoineb19@gmail.com>
2014-09-06 02:52:01 -0700

    Update elasticsearch_http.rb

--- recipes/elasticsearch_http.rb
+++ recipes/elasticsearch_http.rb
@@ -1,3 +1,5 @@
 # Fixes the elasticsearch_http plugin.
 
-cookbook_file "/opt/logstash/lib/logstash/outputs/elasticsearch_http.rb"
+cookbook_file "/opt/logstash/lib/logstash/outputs/elasticsearch_http.rb" do 
+  mode "0644"
+end
